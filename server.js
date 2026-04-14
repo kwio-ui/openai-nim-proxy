3f0d63c6551186433ef130e3064b0eab83a3e634
kwio-ui <kwiokatak@gmail.com>
2026-04-14 09:48:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const MODEL_MAPPING = {
   'gemini-1.6-pro': 'stepfun-ai/step-3.5-flash',
   'gemini-1.7-pro': 'google/gemma-4-31b-it', 
   'gemini-1.8-pro': 'z-ai/glm5',
-  'gemini-1.9-pro': 'qwen/qwen3.5-397b-a17b', 
+  'gemini-1.9-pro': 'z-ai/glm5.1', 
   
 
   // Fast but still good quality  
