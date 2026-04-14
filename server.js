dfd744e70b03d0b7942269a0efd4da62267c3133
kwio-ui <kwiokatak@gmail.com>
2026-04-14 10:00:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ app.post('/v1/chat/completions', async (req, res) => {
       frequency_penalty: 0.0,
       presence_penalty: 0.0,
       stop: null,
-      extra_body: requiresThinking ? { chat_template_kwargs: { thinking: true } } : undefined,
+      extra_body: requiresThinking ? { chat_template_kwargs: {thinking:True, clear_thinking:true, do_sample:True, enable_thinking:True} } : undefined,
       stream: stream || false
     };
     
