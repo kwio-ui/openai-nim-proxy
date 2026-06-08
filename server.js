c6d7aaf6368c090c1018e12723f41503e0e4d497
kwio-ui <kwiokatak@gmail.com>
2026-06-08 23:52:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -154,7 +154,7 @@ app.post('/v1/chat/completions', async function(req, res) {
 
       function closeThinkBlock() {
         if (reasoningOpen && !thinkingClosed) {
-          emitSynthetic('</think>\n\n');
+          emitSynthetic('<think>\n\n');
           thinkingClosed = true;
           reasoningOpen = false;
         }
@@ -206,7 +206,7 @@ app.post('/v1/chat/completions', async function(req, res) {
 
               if (content) {
                 if (reasoningOpen && !thinkingClosed) {
-                  combined += '</think>\n\n' + content;
+                  combined += '<think>\n\n' + content;
                   thinkingClosed = true;
                   reasoningOpen = false;
                 } else {
